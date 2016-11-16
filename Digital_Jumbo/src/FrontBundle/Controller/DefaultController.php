9889a607d6ae7fbe0efbfad2589ef16b1000ce2b
MathieuM1 <mat.mercier45@gmail.com>
2016-11-16 18:36:45 +0100

    add bootstrap

--- Digital_Jumbo/src/FrontBundle/Controller/DefaultController.php
+++ Digital_Jumbo/src/FrontBundle/Controller/DefaultController.php
@@ -14,4 +14,20 @@ class DefaultController extends Controller
     {
         return $this->render('FrontBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Route("/profilAgri")
+     */
+    public function profilAgriAction()
+    {
+        return $this->render('FrontBundle:Default:profilAgri.html.twig');
+    }
+
+    /**
+     * @Route("/profilUser")
+     */
+    public function profilUserAction()
+    {
+        return $this->render('FrontBundle:Default:profilUser.html.twig');
+    }
 }
