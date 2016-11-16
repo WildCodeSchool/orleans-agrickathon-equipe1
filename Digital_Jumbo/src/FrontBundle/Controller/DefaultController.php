5a991d21df4e5620cc3239df66e64e2b89fe6e66
Jasdero <houssemaine.j@gmail.com>
2016-11-16 16:13:50 +0100

    geoloc + lieux

--- Digital_Jumbo/src/FrontBundle/Controller/DefaultController.php
+++ Digital_Jumbo/src/FrontBundle/Controller/DefaultController.php
@@ -12,6 +12,10 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('FrontBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $locations = $em->getRepository('FrontBundle:Site')->findAll();
+
+        return $this->render('FrontBundle:Default:geolocation.html.twig', array('locations'=>$locations));
     }
 }
