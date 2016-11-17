57230e9755aeea4a1fd793a2bebc8f94735f16d7
Jasdero <houssemaine.j@gmail.com>
2016-11-17 09:22:44 +0100

    refonte footer

--- Digital_Jumbo/src/FrontBundle/Controller/DefaultController.php
+++ Digital_Jumbo/src/FrontBundle/Controller/DefaultController.php
@@ -12,11 +12,8 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getManager();
 
-        $locations = $em->getRepository('FrontBundle:Site')->findAll();
-
-        return $this->render('FrontBundle:Default:geolocation.html.twig', array('locations'=>$locations));
+        return $this->render('FrontBundle:Default:accueil.html.twig');
     }
 
     /**
